fb7222cf94854a3b54fb41b843b38efaa4bf24da
Ran Tavory <rantav@gmail.com>
2022-10-16 15:03:03 +0300

    Add ChakraUI

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,5 @@
+import { Heading } from '@chakra-ui/react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
@@ -7,16 +7,8 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <Heading>Team 81's Github Exlorer</Heading>
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -26,7 +18,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </div>
   )
 }
