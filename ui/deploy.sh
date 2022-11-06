5abec462eabd51551c102de755e2e1a42baa8cb5
Ran Tavory <rantav@gmail.com>
2022-11-07 00:01:49 +0200

    Finish up deployment

--- ui/deploy.sh
+++ ui/deploy.sh
@@ -5,10 +5,11 @@ set -e
 currenet_branch=$(git rev-parse --abbrev-ref HEAD)
 
 # build
-npm run build
-
 git checkout gh-pages
+npm run build
 cp -r dist/* ..
+
+git ci -am "deploy"
 
 echo "Pushing to gh-pages"
 # git push -f https://github.com/rantav/cse6242DVA-project.git master:gh-pages
@@ -16,6 +17,7 @@ git push origin gh-pages
 
 git checkout $currenet_branch
 
+echo "Deployed to gh-pages, view at https://rantav.github.io/cse6242DVA-project/"
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
