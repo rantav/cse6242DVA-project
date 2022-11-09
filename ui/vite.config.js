b5d079166e844223d6be6fe5eece05ec96c5b688
Ran Tavory <rantav@gmail.com>
2022-11-09 10:09:37 +0200

    Move FE to _vite

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cse6242DVA-project/'
+  // base: '/cse6242DVA-project/'
+  base: '/_vite/'
 })
