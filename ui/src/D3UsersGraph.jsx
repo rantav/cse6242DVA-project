1a12551487e69e65c02a10a2bf8200efd1c360be
Ran Tavory <rantav@gmail.com>
2022-11-15 11:39:12 +0200

    Implement shortest path query

--- ui/src/D3UsersGraph.jsx
+++ ui/src/D3UsersGraph.jsx
@@ -1,70 +1,6 @@
 import * as d3 from 'd3';
 import ForceGraph from './ForceGraph.js'
 
-// Convert from neo4j format (mock-results-nicer-format.json) to D3 format
-function reshapeForD3(data) {
-  // Type returned from the database now
-  let links = data.map((d) => {
-    return {
-      source: d.d[0].name,
-      target: d.d[2].title,
-      type: d.d[1]
-    }})
-  let nodes = data.map((d, i) => {
-    return {
-      id: d.d[0].name,
-      name: d.d[0].name,
-      group: 'person'
-    }})
-    .concat(data.map((d) => {
-      return {
-        id: d.d[2].title,
-        name: d.d[2].title,
-        group: 'movie'
-      }}))
-
-  // type returned from the mock file
-  // const links = d3.map(data, (d) => {
-  //   return {
-  //     id: d.d.identity,
-  //     source: d.d.start,
-  //     target: d.d.end,
-  //     type: d.d.type,
-  //     properties: d.d.properties
-  //   }});
-  // let nodes = d3.map(data, (d) => {
-  //   return {
-  //     id: d.p.identity,
-  //     labels: d.p.labels,
-  //     properties: d.p.properties,
-  //     group: 1,
-  //   }})
-  //   .concat(d3.map(data, (d) => {
-  //     return {
-  //       id: d.m.identity,
-  //       labels: d.m.labels,
-  //       properties: d.m.properties,
-  //       group: 2,
-  //     }}))
-  // nodes = d3.map(nodes, (n) => {
-  //   return {
-  //     id: n.id,
-  //     labels: n.labels,
-  //     properties: n.properties,
-  //     group: n.group,
-  //     name: n.properties.name}})
-
-  // Unique the nodes
-  nodes = [...new Map(nodes.map(
-    item => [item['id'], item])
-    ).values()]
-  links = [...new Map(links.map(
-    item => [`${item['source']}-${item['target']}-${item['type']}`, item])
-    ).values()]
-  return {nodes, links}
-
-}
-
 
 export default class D3UsersGraph {
 
@@ -77,7 +13,6 @@ export default class D3UsersGraph {
     this.props = props;
     const { data, width, height, onNodeClick } = props;
     this.svg = d3.select(containerEl)
-    const d = reshapeForD3(data);
     this.chart = ForceGraph({
       svg: this.svg,
       nodeId: d => d.id,
@@ -87,19 +22,18 @@ export default class D3UsersGraph {
       linkStrokeWidth: 1,
       nodeRadius: 20,
       nodeStroke: '#eee',
-      nodeStrength: -300,
-      linkDistance: 200,
+      nodeStrength: -800,
+      linkDistance: 100,
       width,
       height,
       onNodeClick,
       // invalidation // a promise to stop the simulation when the cell is re-run
     });
-    this.chart.update(d)
+    this.chart.update(data)
   }
 
   update = (data) => {
-    const d = reshapeForD3(data);
-    this.chart.update(d)
+    this.chart.update(data)
   }
 
   // TODO: Resize still not working
--- ui/src/UsersGraphContainer.jsx
+++ ui/src/UsersGraphContainer.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
 import D3UsersGraph from './D3UsersGraph';
+import {
+  Button,
+  Stack
+} from '@chakra-ui/react'
 
 export default function UsersGraphContainer({setSelectedEntity}) {
   const [data, setData] = useState(null);
@@ -8,19 +12,47 @@ export default function UsersGraphContainer({setSelectedEntity}) {
   const [height, setHeight] = useState(window.innerHeight - 500);
   const refElement = useRef(null);
 
-  useEffect(fetchData, []);
+  useEffect(fetchInitData, []);
   useEffect(initVis, [ data ]);
 
-  function fetchData() {
+  function shortestPath() {
+    const q = `MATCH (KevinB:Person {name: 'Kevin Bacon'} ),
+        (Al:Person {name: 'Al Pacino'}),
+        path = shortestPath((KevinB)-[:ACTED_IN*]-(Al))
+      WHERE all(r IN relationships(path) WHERE r.roles IS NOT NULL)
+      RETURN path`;
+
+    const query = new URLSearchParams({q})
+    const url = '/query?' + query;
+    fetch(url).then(response => {
+      response.json().then(d => {
+        d = reshapeGraphDataPath(d);
+        setData(d)
+      })
+    });
+  }
+
+  function fetchInitData() {
     let query = 'MATCH (p:Person)-[d:DIRECTED]-(m:Movie) where m.released > 2000 RETURN p,d,m';
     query = new URLSearchParams({q: query})
     const url = '/query?' + query;
     fetch(url).then(response => {
       response.json().then(d => {
+        d = reshapeGraphData(d);
         setData(d)
       })
     });
   }
+
+  // function fetchQuery(query) {
+  //   query = new URLSearchParams({q: query})
+  //   const url = '/query?' + query;
+  //   fetch(url).then(response => {
+  //     response.json().then(d => {
+  //       setData(d)
+  //     })
+  //   });
+  // }
 
   function expandNode(n) {
     let query = '';
@@ -33,9 +65,105 @@ export default function UsersGraphContainer({setSelectedEntity}) {
     const url = '/query?' + query;
     fetch(url).then(response => {
       response.json().then(newData => {
-        setData(oldData => newData.concat(oldData))
+        newData = reshapeGraphData(newData);
+        setData(oldData => mergeNodesLinks(newData, oldData))
       })
     });
+  }
+
+  function mergeNodesLinks(d1, d2) {
+    const nodes = d1.nodes.concat(d2.nodes);
+    const links = d1.links.concat(d2.links);
+    return dedupNodesLinks({nodes, links});
+  }
+
+  // Convert from neo4j format to D3 format
+  function reshapeGraphData(data) {
+    let links = data.map((d) => {
+      return {
+        source: d.d[0].name,
+        target: d.d[2].title,
+        type: d.d[1]
+      }})
+    let nodes = data.map((d, i) => {
+      return {
+        id: d.d[0].name,
+        name: d.d[0].name,
+        group: 'person'
+      }})
+      .concat(data.map((d) => {
+        return {
+          id: d.d[2].title,
+          name: d.d[2].title,
+          group: 'movie'
+        }}))
+
+    // type returned from the mock file
+    // const links = d3.map(data, (d) => {
+    //   return {
+    //     id: d.d.identity,
+    //     source: d.d.start,
+    //     target: d.d.end,
+    //     type: d.d.type,
+    //     properties: d.d.properties
+    //   }});
+    // let nodes = d3.map(data, (d) => {
+    //   return {
+    //     id: d.p.identity,
+    //     labels: d.p.labels,
+    //     properties: d.p.properties,
+    //     group: 1,
+    //   }})
+    //   .concat(d3.map(data, (d) => {
+    //     return {
+    //       id: d.m.identity,
+    //       labels: d.m.labels,
+    //       properties: d.m.properties,
+    //       group: 2,
+    //     }}))
+    // nodes = d3.map(nodes, (n) => {
+    //   return {
+    //     id: n.id,
+    //     labels: n.labels,
+    //     properties: n.properties,
+    //     group: n.group,
+    //     name: n.properties.name}})
+
+    return dedupNodesLinks({nodes, links})
+  }
+
+  function reshapeGraphDataPath(data) {
+    const links = [], nodes = [];
+    const isPerson = (e) => !!e.name;
+    const getId = (e) => isPerson(e) ? e.name : e.title;
+    const p = data[0].path;
+    for (let i = 0; i < p.length; i++) {
+
+        if(i % 2 == 0) {
+            nodes.push({
+                name: getId(p[i]),
+                id: getId(p[i]),
+                group: isPerson(p[i]) ? 'person' : 'movie',
+            });
+        } else {
+            links.push({
+                source: getId(p[i - 1]),
+                target: getId(p[i + 1]),
+                type: p[i],
+            });
+        }
+    }
+    return {nodes, links};
+  }
+
+  function dedupNodesLinks({nodes, links}) {
+    nodes = [...new Map(nodes.map(
+      item => [item['id'], item])
+      ).values()]
+    links = [...new Map(links.map(
+      item => [`${item['source']}-${item['target']}-${item['type']}`, item])
+      ).values()]
+    return {nodes, links}
   }
 
   function initVis() {
@@ -61,7 +189,10 @@ export default function UsersGraphContainer({setSelectedEntity}) {
 
   return (
     <div>
-        <button onClick={fetchData}>reset</button>
+        <Stack spacing={4} direction='row' align='center'>
+          <Button onClick={shortestPath}>Shortest Path Demo</Button>
+          <Button onClick={fetchInitData}>Reset</Button>
+        </Stack>
         <svg width={width} height={height} ref={refElement}></svg>
     </div>
   );
